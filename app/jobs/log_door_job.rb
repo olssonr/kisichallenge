057fb34b76b0b3af12f160387a9ab5e944d75a77
Rickard Olsson <rickard@olssonr.com>
2020-09-30 20:51:42 +0200

    Add counting of duration of the jobs

--- app/jobs/log_door_job.rb
+++ app/jobs/log_door_job.rb
@@ -4,6 +4,7 @@ class LogDoorJob < ApplicationJob
   queue_as :default
 
   def perform(door, event)
+    sleep 1
     puts "#{door} was #{event}"
     #raise Exception, "just want to test retries and morgue"
   end
--- config/initializers/events.rb
+++ config/initializers/events.rb
@@ -8,10 +8,15 @@ ActiveSupport::Notifications.subscribe "perform.active_job" do |*args|
   Rails.logger.info "Job: #{event.payload[:job]} performed!"
 
   begin
-    metric = Metric.find_or_initialize_by(name: 'jobs_performed_total_count')
-    metric.value += 1
-    metric.save!
-    Rails.logger.info "Saved: #{metric}"
+    perform_count = Metric.find_or_initialize_by(name: 'jobs_performed_total_count')
+    perform_count.value += 1
+    perform_count.save!
+    Rails.logger.info "Saved: #{perform_count}"
+
+    perform_duration = Metric.find_or_initialize_by(name: 'jobs_performed_total_duration')
+    perform_duration.value += event.duration
+    perform_duration.save!
+    Rails.logger.info "Saved: #{perform_duration}"
   rescue Exception => e
     Rails.logger.info "Got exception: #{e.inspect}"
   end
