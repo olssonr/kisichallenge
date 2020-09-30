66b438febe7bd06c5811f8e2805e2f7b9835452e
Rickard Olsson <rickard@olssonr.com>
2020-09-30 20:36:16 +0200

    Fix so counting performed jobs works

--- config/initializers/events.rb
+++ config/initializers/events.rb
@@ -1,16 +1,18 @@
-
-# This is just a first test with instrumentation
-# TODO: Measuring performed active jobs requires a connection with either the worker or pub/sub so we can now what is actually performed.
 ActiveSupport::Notifications.subscribe "enqueue.active_job" do |*args|
-    event = ActiveSupport::Notifications::Event.new *args
-
-    Rails.logger.info "Job: #{event.payload[:job]} enqueued!"
+  event = ActiveSupport::Notifications::Event.new *args
+  Rails.logger.info "Job: #{event.payload[:job]} enqueued!"
 end
 
-# There exists a perform event but since there is no connection between subscriber and rails application we do not receive this even
-# TODO: Investigate if we want a connection here or if instrumentation should be setup else where and listen to Google pub/sub for example
 ActiveSupport::Notifications.subscribe "perform.active_job" do |*args|
-    event = ActiveSupport::Notifications::Event.new *args
+  event = ActiveSupport::Notifications::Event.new *args
+  Rails.logger.info "Job: #{event.payload[:job]} performed!"
 
-    Rails.logger.info "Job: #{event.payload[:job]} performed!"
+  begin
+    metric = Metric.find_or_initialize_by(name: 'jobs_performed_total_count')
+    metric.value += 1
+    metric.save!
+    Rails.logger.info "Saved: #{metric}"
+  rescue Exception => e
+    Rails.logger.info "Got exception: #{e.inspect}"
+  end
 end
